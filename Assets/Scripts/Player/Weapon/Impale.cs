3cfd60b574a26112e3aaa4e1351dcfddbb23428d
Luciano <luciano.rosset@rappi.com>
2021-06-12 03:31:11 -0300

    shishkebab!

--- Assets/Scripts/Player/Weapon/Impale.cs
+++ Assets/Scripts/Player/Weapon/Impale.cs
@@ -4,28 +4,15 @@ using UnityEngine;
 
 public class Impale : MonoBehaviour
 {
-    
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnCollisionEnter2D(Collision2D other)
     {
         EnemyDeath enemyDeath = other.gameObject.GetComponent<EnemyDeath>();
         if (!enemyDeath)
             return;
         GameObject newWeaponPiece = enemyDeath.SpawnDeadEnemy();
-        FixedJoint2D myJoint = gameObject.AddComponent<FixedJoint2D>();
-        myJoint.anchor = other.contacts[other.contacts.Length - 1].point;
-        myJoint.connectedBody = newWeaponPiece.GetComponent<Rigidbody2D>();
+        Transform newTransform = newWeaponPiece.transform;
+        newTransform.position = transform.position + transform.localToWorldMatrix.MultiplyVector(Vector3.right);
+        newTransform.rotation = transform.rotation;
+        newTransform.SetParent(transform);
     }
 }
