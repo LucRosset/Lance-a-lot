dd1d6ee5fa6696c0fdb0dd0344b3fb8cdd95826c
Luciano <luciano.rosset@rappi.com>
2021-06-11 23:54:35 -0300

    pirocoptero

--- Assets/Scripts/Player/Lance/MoveLance.cs
+++ Assets/Scripts/Player/Lance/MoveLance.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class MoveLance : MonoBehaviour
 {
+    [SerializeField] private float rotationTorque = 1f;
+
+    private Rigidbody2D myRigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButton("CounterClockwise"))
+            Rotate(1f);
+        if (Input.GetButton("Clockwise"))
+            Rotate(-1f);
+    }
+
+    private void Rotate(float direction)
+    {
+        myRigidbody.AddTorque(rotationTorque * direction * Time.deltaTime);
     }
 }
