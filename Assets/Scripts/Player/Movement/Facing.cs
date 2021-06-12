838baf4a6675ae95fcf1b5a55ea5f6da9e8f8ef7
Luciano <luciano.rosset@rappi.com>
2021-06-12 17:11:38 -0300

    facing

---
+++ Assets/Scripts/Player/Movement/Facing.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Facing : MonoBehaviour
+{
+    [SerializeField] private float minVelocity = 1f;
+
+    private Rigidbody2D myRigidbody;
+    private SpriteRenderer myRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Flip from right to left
+        if (!myRenderer.flipX && myRigidbody.velocity.x < -minVelocity)
+            myRenderer.flipX = true;
+        // Flip from left to right
+        else if (myRenderer.flipX && myRigidbody.velocity.x > minVelocity)
+            myRenderer.flipX = false;
+    }
+}
