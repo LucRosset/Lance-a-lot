e54ad46e2e784872de9c6db1880ac2401598c31f
Luciano <luciano.rosset@rappi.com>
2021-06-12 10:00:19 -0300

    Só a ponta machuca ;)

--- Assets/Scripts/Player/Weapon/Impale.cs
+++ Assets/Scripts/Player/Weapon/Impale.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Impale : MonoBehaviour
 {
+    [SerializeField] private float impaleDistance = 1f;
+    public PlayerPivot pivot {set; get;}
+    public Impale previousElement {set; get;}
+    public Impale nextElement {set; get;}
+
+    void Start()
+    {
+        if (tag == "Lance")
+            pivot = GetComponent<PlayerPivot>();
+        Debug.Log("pivot", pivot.lastElement.gameObject);
+    }
+
     private void OnCollisionEnter2D(Collision2D other)
     {
         EnemyDeath enemyDeath = other.gameObject.GetComponent<EnemyDeath>();
-        if (!enemyDeath)
+        if (!enemyDeath || pivot.lastElement.GetInstanceID() != transform.GetInstanceID())
             return;
+
         GameObject newWeaponPiece = enemyDeath.SpawnDeadEnemy();
         Transform newTransform = newWeaponPiece.transform;
-        newTransform.position = transform.position + transform.localToWorldMatrix.MultiplyVector(Vector3.right);
-        newTransform.rotation = transform.rotation;
-        newTransform.SetParent(transform);
+        newTransform.position = pivot.lastElement.position
+            + pivot.lastElement.localToWorldMatrix.MultiplyVector(impaleDistance * Vector3.right);
+        newTransform.rotation = pivot.lastElement.rotation;
+        newTransform.SetParent(pivot.lastElement);
+
+        pivot.lastElement = newTransform;
+        nextElement = newWeaponPiece.GetComponent<Impale>();
+        nextElement.pivot = pivot;
+        nextElement.previousElement = this;
     }
 }
---
+++ Assets/Scripts/Player/Weapon/PlayerPivot.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPivot : MonoBehaviour
+{
+    public Transform lastElement {set; get;}
+
+    void Start()
+    {
+        lastElement = transform;
+    }
+}
