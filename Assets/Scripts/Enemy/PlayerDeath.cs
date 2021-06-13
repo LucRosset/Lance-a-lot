31528a3b54c6facdbf5970080c3c3e19471c522c
Luciano <luciano.rosset@rappi.com>
2021-06-13 03:19:32 -0300

    Enough...

---
+++ Assets/Scripts/Enemy/PlayerDeath.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour
+{
+    private SceneLoader sceneLoader;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        sceneLoader.Restart(2f);
+    }
+}
